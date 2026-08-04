bbec3f723e5ffdbe32a8734ed0d9ad00bb5a871a
stbz <ympt@qq.com>
2026-08-04 19:47:24 +0800

    Advance SaaS baseline to official 45eab11d

--- tests/saas/test_control_plane_resolver.py
+++ tests/saas/test_control_plane_resolver.py
@@ -31,7 +31,7 @@ from saas.control_plane.db_models import ProjectRecord
 COMPATIBILITY_POLICY = RuntimeCompatibilityPolicy(
     runtime_type="omnigent",
     allowed_runtime_versions=frozenset(("0.9.0.dev0",)),
-    allowed_source_revisions=frozenset(("b8fd1952ac77b8c78ef971de801f2d35379c3b3b",)),
+    allowed_source_revisions=frozenset(("45eab11d531f3224eb59b39da7d8cb18256e21a1",)),
     allowed_schema_revisions=frozenset(("c4d5e6f7a8b9",)),
     adapter_contract_version="0.2.0",
 )
@@ -182,7 +182,7 @@ def control_plane() -> tuple[sessionmaker[Session], SeededScope]:
                 runtime_version="0.9.0.dev0",
                 physical_partition_key="41",
                 placement_generation=4,
-                source_revision="b8fd1952ac77b8c78ef971de801f2d35379c3b3b",
+                source_revision="45eab11d531f3224eb59b39da7d8cb18256e21a1",
                 adapter_contract_version="0.2.0",
                 status="active",
             )
